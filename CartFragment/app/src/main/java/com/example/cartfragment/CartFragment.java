53ba8869973cb3c7cf5d56b8ca13d64118b7d0c5
hoanghiep0912 <hiephoangdang09122004@gmail.com>
2025-04-14 22:40:48 +0700

    hiep

--- CartFragment/app/src/main/java/com/example/cartfragment/CartFragment.java
+++
@@ -1,4 +0,0 @@
-package com.example.cartfragment;
-
-public class CartFragment {
-}
--- HelloConstrain-master/app/src/main/java/com/example/helloconstrain/MainActivity.java
+++ HelloConstrain-master/app/src/main/java/com/example/helloconstrain/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+//import androidx.core.view.WindowInsetsControllerCompat;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mShowCount;
